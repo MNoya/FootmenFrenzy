5e1d87a7f2fda38a582cb2bcbd447105ed356dec
John <john.oberhauser@live.com>
2015-06-18 07:35:37 -0500

    work work

--- game/dota_addons/ffrenzy/scripts/vscripts/ffrenzy.lua
+++ game/dota_addons/ffrenzy/scripts/vscripts/ffrenzy.lua
@@ -13,7 +13,7 @@ UNIVERSAL_SHOP_MODE = false             -- Should the main shop contain Secret S
 ALLOW_SAME_HERO_SELECTION = true        -- Should we let people select the same hero as each other
 
 HERO_SELECTION_TIME = 1.0              -- How long should we let people select their hero?
-PRE_GAME_TIME = 0.0                    -- How long after people select their heroes should the horn blow and the game start?
+PRE_GAME_TIME = 30.0                    -- How long after people select their heroes should the horn blow and the game start?
 POST_GAME_TIME = 60.0                   -- How long should we let people look at the scoreboard before closing the server automatically?
 TREE_REGROW_TIME = 60.0                 -- How long should it take individual trees to respawn after being cut down/destroyed?
 
@@ -77,6 +77,8 @@ function GameMode:InitGameMode()
     self.m_NumAssignedPlayers = 0
 
     GameMode:GatherValidTeams()
+	
+	
     -----------------------
 
     -- Setup rules
@@ -374,7 +376,7 @@ function GameMode:OnGameRulesStateChange(keys)
     --DeepPrintTable(keys)
 	
 	if nNewState == DOTA_GAMERULES_STATE_CUSTOM_GAME_SETUP then
-		
+	
 	end
 
     -- MultiTeam Thinker
