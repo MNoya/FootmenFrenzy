1c9508740bceb4651254eb556842b817bd1946bb
John <john.oberhauser@live.com>
2015-06-24 23:16:50 -0500

    pulverize, ui bug, cannon tower aoe

--- game/dota_addons/ffrenzy/scripts/vscripts/ffrenzy.lua
+++ game/dota_addons/ffrenzy/scripts/vscripts/ffrenzy.lua
@@ -53,7 +53,7 @@ local team_c_counter = 0
 local team_d_counter = 0
 
 -- Change these at will
-local testing = true
+local testing = false
 local testingUnits = false
 local testingLevels = false
 
