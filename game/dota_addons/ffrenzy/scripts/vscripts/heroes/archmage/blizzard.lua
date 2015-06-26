0fb5ec5058cad8327de7f39e59881c48fdfcb7ce
MNoya <martinnoya@gmail.com>
2015-06-26 17:35:12 -0300

    Fixed Blizzard animation causing a sure crash if fully channeled

--- game/dota_addons/ffrenzy/scripts/vscripts/heroes/archmage/blizzard.lua
+++ game/dota_addons/ffrenzy/scripts/vscripts/heroes/archmage/blizzard.lua
@@ -5,13 +5,13 @@
 ]]
 function BlizzardStart( event )
 	-- Variables
-	print("blizzar start called")
 	local caster = event.caster
 	local point = event.target_points[1]
-	print(point)
 
 	caster.blizzard_dummy = CreateUnitByName("dummy_unit_vulnerable", point, false, caster, caster, caster:GetTeam())
 	event.ability:ApplyDataDrivenModifier(caster, caster.blizzard_dummy, "modifier_blizzard_thinker", nil)
+
+	DebugDrawCircle(point, Vector(0,255,0), 255, 100, true, 8)
 end
 
 -- -- Create the particles with small delays between each other
@@ -31,7 +31,7 @@ function BlizzardWave( event )
 
     Timers:CreateTimer(0.05,function()
     local particle2 = ParticleManager:CreateParticle( particleName, PATTACH_CUSTOMORIGIN, caster )
-	 ParticleManager:SetParticleControl( particle2, 0, target_position+RandomVector(100) ) end)
+	ParticleManager:SetParticleControl( particle2, 0, target_position+RandomVector(100) ) end)
 
     Timers:CreateTimer(0.1,function()
 	local particle3 = ParticleManager:CreateParticle( particleName, PATTACH_CUSTOMORIGIN, caster )
@@ -44,12 +44,23 @@ function BlizzardWave( event )
     Timers:CreateTimer(0.2,function()
 	local particle5 = ParticleManager:CreateParticle( particleName, PATTACH_CUSTOMORIGIN, caster )
 	 ParticleManager:SetParticleControl( particle5, 0, target_position-RandomVector(RandomInt(50,100)) ) end)
+end
 
-    print(target_position)
+function ApplyAnimation( event )
+	local ability = event.ability
+	local caster = event.caster
+	local start_time = ability:GetChannelStartTime()
+	local time_channeled = GameRules:GetGameTime() - start_time
+	local max_channel_time = ability:GetLevelSpecialValueFor("wave_duration", ability:GetLevel() - 1)
+
+	if ability:IsChanneling() and (time_channeled < max_channel_time - 1) then
+		ability:ApplyDataDrivenModifier(caster, caster, "modifier_channeling", { duration = 0.9 })
+	end
 end
 
 function BlizzardEnd( event )
 	local caster = event.caster
-	--caster.blizzard_dummy:RemoveModifierByName("modifier_blizzard_thinker")
-	--caster.blizzard_dummy:RemoveSelf()
+	caster.blizzard_dummy:RemoveModifierByName("modifier_blizzard_thinker")
+	caster.blizzard_dummy:RemoveSelf()
+	caster:RemoveModifierByName("modifier_blizzard_channelling")
 end
