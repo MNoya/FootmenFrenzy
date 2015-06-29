b3ed7e966da9cea61adefd9626ef9175c1e2f5d3
John <john.oberhauser@live.com>
2015-06-28 22:17:13 -0500

    changes/fixes so far

--- game/dota_addons/ffrenzy/scripts/vscripts/units/raise_dead.lua
+++ game/dota_addons/ffrenzy/scripts/vscripts/units/raise_dead.lua
@@ -1,26 +1,48 @@
 function RaiseDead( event )
+	local caster = event.caster
+	local ability = event.ability
+
+	local pID = caster:GetPlayerOwnerID()
+	local team = caster:GetTeamNumber()
+	local duration = ability:GetLevelSpecialValueFor( "duration", ability:GetLevel() - 1 )
+	--local fv = caster:GetForwardVector()
+	--local origin = caster:GetAbsOrigin()
+	--local position = origin + fv * 200
+	
+	
 	Timers:CreateTimer(0.1,function ()
-		local caster = event.caster
-		local ability = event.ability
+		print("Timer a")
+		if ability:GetCooldownTimeRemaining() == 0 and (caster:IsIdle() or caster:IsAttacking()) then
+			print("Timer b")
+			-- Find all corpse entities in the radius
+			local targets = Entities:FindAllByNameWithin("npc_dota_creature", caster:GetAbsOrigin(), ability:GetCastRange())
+			-- Go through every unit, stop at the first corpse found
+			for _, unit in pairs(targets) do
+				if unit.corpse_expiration ~= nil then
+					
+					
+					ability:ApplyDataDrivenModifier(caster, caster, "modifier_raise_dead_cast_animation", nil)
+				
+					-- Create the skeleton
+					local skeleton = CreateUnitByName("undead_skeletal_mage_skeleton_warrior", unit:GetAbsOrigin(), true, caster, caster, team)
+					skeleton:SetControllableByPlayer(pID, true)
+					FindClearSpaceForUnit(skeleton, unit:GetAbsOrigin(), true)
 
-		local pID = caster:GetPlayerOwnerID()
-		local team = caster:GetTeamNumber()
-		local duration = ability:GetLevelSpecialValueFor( "duration", ability:GetLevel() - 1 )
-		local fv = caster:GetForwardVector()
-		local origin = caster:GetAbsOrigin()
-		local position = origin + fv * 200
+					-- Apply modifiers for the summon properties
+					skeleton:AddNewModifier(caster, ability, "modifier_kill", {duration = duration})
+					ability:ApplyDataDrivenModifier(caster, skeleton, "modifier_raise_dead", nil)
 
-		local skeleton = CreateUnitByName("undead_skeletal_mage_skeleton_warrior", position, true, caster, caster, team)
-		skeleton:SetControllableByPlayer(pID, true)
-		FindClearSpaceForUnit(skeleton, position, true)
-
-		-- Apply modifiers for the summon properties
-		skeleton:AddNewModifier(caster, ability, "modifier_kill", {duration = duration})
-		ability:ApplyDataDrivenModifier(caster, skeleton, "modifier_raise_dead", nil)
-
-		-- Leave no corpses
-		skeleton.no_corpse = true
-
-		ability:StartCooldown(ability:GetCooldown(1))
+					-- Leave no corpses
+					skeleton.no_corpse = true
+		
+					ability:StartCooldown(ability:GetCooldown(1))
+					return 60
+				end
+			end
+		end
+		
+		
+		return 1
+		
 	end)
 end
