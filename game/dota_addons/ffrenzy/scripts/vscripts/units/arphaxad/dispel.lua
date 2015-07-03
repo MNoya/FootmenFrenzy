f7eeb43d03277c88a5e3ba73e21dff65c83cffd2
John <john.oberhauser@live.com>
2015-07-03 03:48:43 -0500

    rebuild map
    -OCD map changes
    -workin on priest creep

--- game/dota_addons/ffrenzy/scripts/vscripts/units/arphaxad/dispel.lua
+++ game/dota_addons/ffrenzy/scripts/vscripts/units/arphaxad/dispel.lua
@@ -1,3 +1,20 @@
 function Cast (event)
-
+	local caster = event.caster
+	local ability = event.ability
+	local targets = event.target_entities
+	local teamID = caster:GetTeamNumber()
+	local radius = ability:GetLevelSpecialValueFor( "radius", ability:GetLevel() - 1 )
+	
+	for _,unit in pairs (targets) do
+		if unit:GetTeamNumber() == teamID then
+			print("ally")
+			unit:Purge(false, true, false, false, false)
+		else
+			print("enemy")
+			unit:Purge(true, false, false, false, false)
+		end
+		if unit:IsSummoned() then
+			ApplyDamage({ victim = unit, attacker = event.target, damage = damage, damage_type = abilityDamageType}) 
+		end
+	end
 end
